3a83921066504702375568a011eed5f996e1447b
Derich Pacheco <d.erich@hotmail.com>
2023-06-30 15:43:35 -0300

    fix: favor initializer over settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,10 +53,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Setup resend as the email method
   config.action_mailer.delivery_method = :resend
-  config.action_mailer.resend_settings = {
-    api_key: ENV["RESEND_API_KEY"],
-  }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
