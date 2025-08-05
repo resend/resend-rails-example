d0f6a55dbe172a0a185f736dbdf84148baa2fef8
Derich Pacheco <carlosderich@gmail.com>
2025-08-04 23:18:53 -0300

    feat: inline attachment example

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,15 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
+
     attachments["invoice.pdf"] = File.read(Rails.root.join("resources","invoice.pdf"))
+    attachments.inline["inline-image"] = File.read(Rails.root.join("resources","image.png"))
+
     @url  = "http://example.com/login"
     mail(
       to: ["delivered@resend.dev"],
       reply_to: "delivered@resend.dev",
-      subject: "Hello from Rails",
+      subject: "Hello from Resend Ruby SDK",
       tags: {
         "name": "category", "value": "confirm_email"
       },
@@ -17,7 +20,7 @@ class UserMailer < ApplicationMailer
         "X-Entity-Ref-ID": "123",
       },
       options: {
-        idempotency_key: "99a",
+        idempotency_key: "hh11bfasd",
       }
     )
   end
